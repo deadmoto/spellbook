d1be201a6c4384357a5df841772407d632a21f94
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-07-24 11:36:51 -0700

    Update Deluge readme.

---
+++ docker/deluge/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+docker build --tag deluge .
+docker stop deluge
+docker rm deluge
+docker run -it \
+           -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
+           -p 127.0.0.1:8112:8112 \
+           --name deluge \
+           deluge
