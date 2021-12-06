77407150301dde64206633564bdc9eb712a172d8
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-12-06 12:43:45 -0800

    Update Deluge image.

--- docker/deluge/build.sh
+++ docker/deluge/build.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+ #!/usr/bin/env sh
+
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 docker build --tag deluge .
 docker run -it \
            -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
-           --expose=6881-6891 \
+           -p 0.0.0.0:6891:6891 \
            -p 127.0.0.1:8112:8112 \
-           --name deluge-$(date +%Y%m%d%H%M%S) \
+           --name deluge-$TIMESTAMP \
            --restart always \
            deluge
