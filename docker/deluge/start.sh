884383175149ebf4526c68ddf081ec4bbafe1118
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-12-06 11:25:31 -0800

    Remove unused shell script.

--- docker/deluge/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-docker start -a deluge
-# /root/.config/deluge/core.conf
-# "download_location": "/Users/dberezhnoy/Downloads/",
-# "move_completed_path": "/root/Downloads"
-# "torrentfiles_location": "/root/Downloads",
