044835a9225e29f57a50714aa3747a84de1bfa03
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-12-05 18:56:49 -0800

    Update Deluge image.

--- docker/deluge/build.sh
+++ docker/deluge/build.sh
@@ -5,6 +5,7 @@ docker stop deluge
 docker rm deluge
 docker run -it \
            -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
+           -p 127.0.0.1:6881:6881 \
            -p 127.0.0.1:8112:8112 \
            --name deluge \
            --restart always \
