3a1d6f74b0789c9da9543982e981b68c94d8edeb
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-12-06 11:25:02 -0800

    Expose Deluge ports.

--- docker/deluge/build.sh
+++ docker/deluge/build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
 docker build --tag deluge .
-docker stop deluge
-docker rm deluge
 docker run -it \
            -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
-           -p 127.0.0.1:6881:6881 \
+           --expose=6881-6891 \
            -p 127.0.0.1:8112:8112 \
-           --name deluge \
+           --name deluge-$(date +%Y%m%d%H%M%S) \
            --restart always \
            deluge
