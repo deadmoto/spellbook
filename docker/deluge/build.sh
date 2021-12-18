3443d4d89343e4b5ade73fb7f110009157d65f15
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2021-12-18 11:02:55 -0800

    Update Deluge script.

--- docker/deluge/build.sh
+++ docker/deluge/build.sh
@@ -1,12 +1,12 @@
- #!/usr/bin/env sh
+#!/usr/bin/env sh
 
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 docker build --tag deluge .
 docker run -it \
-           -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
-           -p 0.0.0.0:6891:6891 \
-           -p 127.0.0.1:8112:8112 \
-           --name deluge-$TIMESTAMP \
-           --restart always \
-           deluge
+  -v /Users/dberezhnoy/Downloads/:/root/Downloads/:cached \
+  -p 0.0.0.0:6891:6891 \
+  -p 127.0.0.1:8112:8112 \
+  --name deluge-$TIMESTAMP \
+  --restart always \
+  deluge
