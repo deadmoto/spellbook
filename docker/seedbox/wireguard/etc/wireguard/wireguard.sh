f9d6967da6183defabb9484d492ab4e564a95f4a
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2022-01-13 11:02:17 -0800

    Add iptables logging for dropped packets.

--- docker/seedbox/wireguard/etc/wireguard/wireguard.sh
+++ docker/seedbox/wireguard/etc/wireguard/wireguard.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env sh
 
-iptables -F
+ulogd --daemon
+cat /var/log/ulogd.log
 
-iptables -P OUTPUT DROP
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp -d 152.67.230.10 --dport 51820 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -F
 
 iptables -P INPUT DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 8112 -j ACCEPT # Deluge - Web UI
 iptables -A INPUT -p tcp --dport 6891 -j ACCEPT
 iptables -A INPUT -p udp --dport 6891 -j ACCEPT
-iptables -A INPUT -p tcp --dport 8112 -j ACCEPT
+iptables -A INPUT -j NFLOG --nflog-group 0 --nflog-prefix "INPUT dropped: "
 
 iptables -P FORWARD DROP
+iptables -A FORWARD -j NFLOG --nflog-group 0 --nflog-prefix "FORWARD dropped: "
+
+iptables -P OUTPUT DROP
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p udp --sport 6891 -j DROP # Deluge - BitTorrent
+iptables -A OUTPUT -o eth0 -p udp --dport 5351 -j DROP # Deluge - PCP / NAP Port Mapping
+iptables -A OUTPUT -o eth0 -p udp --dport 1900 -j DROP # Deluge - UPnP
+iptables -A OUTPUT -o eth0 -p udp --dport 6771 -j DROP # Deluge - BitTorrent Local Peer Discovery
+iptables -A OUTPUT -o eth0 -d 224.0.0.0/4 -j DROP      # Deluge - IGMP Multicast
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -p udp -d 152.67.230.10 --dport 51820 -j ACCEPT
+iptables -A OUTPUT -j NFLOG --nflog-group 0 --nflog-prefix "OUTPUT dropped: "
 
 wg-quick up wg0
-
-tail -f /dev/null
+tail -f /var/log/ulogd_syslogemu.log
