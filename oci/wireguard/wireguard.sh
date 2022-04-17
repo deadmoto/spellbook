94d0c7c88047bfff8c27b7b633623b41ad11e8a4
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2022-04-16 17:22:19 -0700

    Set up forwarding BIND server.

--- oci/wireguard/wireguard.sh
+++ oci/wireguard/wireguard.sh
@@ -6,25 +6,41 @@ dnf install oraclelinux-developer-release-el8
 dnf config-manager --disable ol8_developer
 dnf config-manager --enable ol8_developer_UEKR6
 dnf config-manager --save --setopt=ol8_developer_UEKR6.includepkgs='wireguard-tools*'
-dnf install -y wireguard-tools
+dnf install -y bind wireguard-tools
 
 echo Enable NAT
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
 sysctl -p
 
+echo Enable Bind DNS server
+echo "
+options {
+	listen-on port 53 { 172.16.0.1; };
+	directory "/var/named";
+  allow-query { any; };
+	recursion yes;
+  forwarders {
+    8.8.8.8;
+    8.8.4.4;
+  };
+  forward only;
+  dnssec-enable yes;
+  dnssec-validation yes;
+};
+" > /etc/named.conf
+systemctl enable named
+systemctl start named
+firewall-offline-cmd --zone=trusted --add-port=53/udp
+
 echo Enable IP masquerade
 firewall-offline-cmd --zone=public --add-masquerade
 
-echo Enable Deluge TCP port 6891
+echo Forward incoming Deluge traffic
+firewall-offline-cmd --zone=public --add-forward-port=port=8112:proto=tcp:toport=8112:toaddr=172.16.0.2
 firewall-offline-cmd --zone=public --add-forward-port=port=6891:proto=tcp:toport=6891:toaddr=172.16.0.2
-
-echo Enable Deluge UDP port 6891
 firewall-offline-cmd --zone=public --add-forward-port=port=6891:proto=udp:toport=6891:toaddr=172.16.0.2
 
-echo Enable Deluge TCP port 8112
-firewall-offline-cmd --zone=public --add-forward-port=port=8112:proto=tcp:toport=8112:toaddr=172.16.0.2
-
-echo Enable Wireguard UDP port 51820
+echo Allow Wireguard server traffic
 firewall-offline-cmd --zone=public --add-port=51820/udp
 
 echo Apply firewalld settings
