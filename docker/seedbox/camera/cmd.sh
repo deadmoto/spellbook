14c200c4d6f455375769a3b49e6021a94d371fed
Dmitry Berezhnoy <deadmoto@gmail.com>
2022-05-15 23:43:21 -0700

    Increase audio buffer.

--- docker/seedbox/camera/cmd.sh
+++ docker/seedbox/camera/cmd.sh
@@ -3,7 +3,7 @@
 mkfifo /tmp/video.ts
 
 ffmpeg -f v4l2 -framerate 4 -video_size 1280x720 -input_format h264 -i /dev/video0 \
-       -f alsa -channels 1 -sample_rate 44100 -itsoffset 1 -i hw:2,0 \
+       -f alsa -channels 1 -sample_rate 44100 -itsoffset 1 -thread_queue_size 1024 -i hw:2,0 \
        -c:v copy \
        -c:a aac -ac 1 -b:a 32k \
        -f mpegts -y /tmp/video.ts &
