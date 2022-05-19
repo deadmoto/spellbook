1d4bd85310942686085ef9cbb7e1f2ad9f0232aa
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2022-05-19 08:58:16 -0700

    Create a new temporary file every time container starts.

--- docker/seedbox/camera/cmd.sh
+++ docker/seedbox/camera/cmd.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-mkfifo /tmp/video.ts
+VIDEO_FIFO=$(mktemp)
+mkfifo $VIDEO_FIFO
 
+echo "Streaming will begin in 60 seconds" & \
+sleep 60 & \
 ffmpeg -f v4l2 -framerate 4 -video_size 1280x720 -input_format h264 -i /dev/video0 \
        -f alsa -channels 1 -sample_rate 44100 -itsoffset 1 -thread_queue_size 1024 -i hw:2,0 \
        -c:v copy \
        -c:a aac -ac 1 -b:a 32k \
-       -f mpegts -y /tmp/video.ts &
-cvlc stream:///tmp/video.ts --sout '#standard{access=http,mux=ts,dst=:8080}' --sout-http-user pi --sout-http-pwd cam
+       -f mpegts -y $VIDEO_FIFO &
+cvlc stream://$VIDEO_FIFO --sout '#standard{access=http,mux=ts,dst=:8080}' --sout-http-user pi --sout-http-pwd cam
