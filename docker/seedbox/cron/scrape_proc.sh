e7026a51d9fc5924009ac4cf2da808b1cbdf6c57
Dmitry Berezhnoy <deadmoto@gmail.com>
2022-10-02 00:15:25 -0700

    Remove Docker files.

--- docker/seedbox/cron/scrape_proc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo Export CPU usage
-mkdir -p /var/local/node_exporter
-ps ax -o %cpu,comm h --sort -%cpu \
-  | grep -v 0.0 \
-  | grep -v kworker \
-  | sed -ne 's/\s\?\([0-9.]\+\)\s\+\(.*\)/node_process_cpu_percent{process="\2"} \1/p' \
-  > /var/local/node_exporter/process_cpu.prom
